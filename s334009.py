afffd2f0a7f6ab08facbbf93120d25f2a7fcf167
nikkola2812 <169174111+nikkola2812@users.noreply.github.com>
2025-02-09 17:15:20 +0100

    Add files via upload

---
+++ s334009.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+#Computational Intelligence
+#Labs and Project Report
+#Nikola Stankovic
+#Politecnico di Torino
+#s334009@studenti.polito.it
+
+
+import numpy as np
+import math
+
+# List of problem files
+#files = ['problem_0.npz','problem_1.npz','problem_2.npz','problem_3.npz','problem_4.npz','problem_5.npz','problem_6.npz','problem_7.npz','problem_8.npz']
+
+def function_0(x):
+    y = x[0] + 0.2 * np.sin(x[1])
+    return y
+
+def function_1(x):
+    y = np.sin(x[0])
+    return y
+
+def function_2(x):
+    y = 7.643021e+06 * np.sin((2*x[0] + x[1] + x[2])/4)
+    return y
+
+def function_3(x):
+    y = 4 - 3.5 * x[2] + 2 * x[0]**2 - x[1]**3
+    return y
+
+def function_4(x):
+    y = 3.279416504354730 - 1/11 * x[0] + 7 * np.cos(x[1])
+    return y
+
+def function_5(x):
+    y=-0.1*x[0]**x[1]+1.624224290224829
+    y= y / 1e+10 
+    return y
+
+def function_6(x):
+    y = 0.694520400387519*(x[1] - x[0]) + x[1]
+    return y
+
+def function_7(x):
+    xm=x[1]-x[0]
+    xp=x[1]+x[0]
+    y=np.log(np.abs(xm))
+    x=xp
+    y=((1.3117133 + (2.0538354 * ((((np.sin((y / 1.1716985) - (0.1342675 * (1.0061529 * (1.0351123 * 1.0061529)))) / 1.0022914) + 0.44840398) - y) * (((((((((1.6232266 * y) / 0.69930816) + x) - 0.78008026) * np.sin(np.exp(0.26678434 * (y * -0.90160197)) - (0.3687389 / np.cosh(x)))) - (((y + x) / 0.81578636) + (-0.5447407 * (0.11499959 * y)))) * -0.0045083454) + np.cosh(x)) + np.cosh(x))))) - (np.cosh(-0.12136203) - ((-0.00042023603 * 0.94144845) / np.exp(y)))) + (y / (((0.26970777 + ((-0.25914347 + (((1.465689 - y) / np.cosh(x)) + (0.612497 * 0.79872614))) / -0.6667237)) / np.sin((y / 1.1129313) + y)) * ((-1.1020997 + -0.970441) - 0.0116060935)))
+    return y    
+
+def function_8(x):
+    y = x[0] - 2*x[1]**2 + 3*x[3]**3 - 4*x[4]**4 + 5*x[5]**5
+    return y
+
+
+
+
+
